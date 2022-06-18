382c91250bb5671a0c35f9369db93af2507da0fd
lidar <97009593+lidarbtc@users.noreply.github.com>
2022-06-18 11:11:11 +0900

    delete

--- dblife/accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import register, login, logout
-urlpatterns = [
-    path('register/', register, name='register'),
-    path('login/', login, name='login'),
-    path('logout/', logout, name='logout'),
-]
--- dblife/tag/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TagConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tag'
